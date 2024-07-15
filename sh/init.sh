ed53163586b48f49f2c56cd02abc651b8150f66f
尤雨东 <root@ltpp.vip>
2024-07-15 23:37:40 +0800

    feat:update

--- sh/init.sh
+++ sh/init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 git remote add ltpp ssh://git@git.ltpp.vip:66/root/LTPP-APP-Flutter.git;
 git remote add github git@github.com:ltpp-universe/LTPP-APP-Flutter.git;
+git remote add origin git@github.com:ltpp-universe/LTPP-APP-Flutter.git;
