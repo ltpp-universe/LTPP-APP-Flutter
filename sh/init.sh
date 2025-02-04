af6b42992d6140c1a9b2ef24f5c2653bc20084c7
尤雨东 <root@ltpp.vip>
2025-02-04 10:01:35 +0800

    feat:

--- sh/init.sh
+++ sh/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git remote add jihulab ssh://jihulab.com:ltpp/LTPP-APP-Flutter.git;
+git remote add jihulab git@jihulab.com:ltpp-universe/LTPP-APP-Flutter.git;
 git remote add github git@github.com:ltpp-universe/LTPP-APP-Flutter.git;
 git remote add origin git@github.com:ltpp-universe/LTPP-APP-Flutter.git;
