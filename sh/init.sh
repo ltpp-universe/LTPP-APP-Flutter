c1ea825465869600a50aed1bde46051aaadc6b58
尤雨东 <root@ltpp.vip>
2025-02-07 23:09:58 +0800

    feat:

--- sh/init.sh
+++ sh/init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git init;
+git config core.ignorecase false;
 git remote add gitee git@gitee.com:ltpp-universe/LTPP-APP-Flutter.git;
-git remote add github git@github.com:ltpp-universe/LTPP-APP-Flutter.git;
 git remote add origin git@github.com:ltpp-universe/LTPP-APP-Flutter.git;
